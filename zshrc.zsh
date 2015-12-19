3ecf47957f90e1f7327c3bf84fea669837566d1c
Adam Demasi <adam@hbang.com.au>
2015-12-19 19:56:50 +1030

    clean up zshrc. add syntax highlighting, run-help, aliases, rm xcopen

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,6 +1,11 @@
-# path yo
-[[ -z "$THEOS" ]] && export THEOS=/opt/theos
+# == REQUIRES: ==
+# git clone --recursive https://github.com/kirb/oh-my-zsh.git ~/.oh-my-zsh
+# brew tap homebrew/command-not-found
+# brew install zsh-syntax-highlighting
+# iTerm2 –> Install Shell Integration
 
+# path yo
+export THEOS=/opt/theos
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:/usr/bin:/bin:/usr/sbin:/sbin:$THEOS/bin:$HOME/.rvm/bin
 export MANPATH=/usr/local/share/man:$MANPATH
 
@@ -8,10 +13,9 @@ export MANPATH=/usr/local/share/man:$MANPATH
 export LANG=en_AU.UTF-8 LC_CTYPE=en_AU.UTF-8
 
 # tmux
-[[ ! -z "$SSH_CLIENT" && -z "$TMUX" ]] && (tmux attach || tmux)
-
-# ssh
-ssh-add >/dev/null
+if [[ ! -z "$SSH_CLIENT" && -z "$TMUX" ]]; then
+	tmux attach || tmux
+fi
 
 # oh my zsh
 ZSH_THEME=kirb-powerlevel
@@ -19,32 +23,78 @@ DISABLE_AUTO_UPDATE=true
 COMPLETION_WAITING_DOTS=true
 ENABLE_CORRECTION=true
 DEFAULT_USER=kirb
-plugins=(battery brew pod safe-paste tmux)
+plugins=(battery brew pod safe-paste ssh-agent tmux)
 
 source $ZSH/oh-my-zsh.sh
 
 # other sources
-source ~/.iterm2_shell_integration.zsh
+safe_source() {
+	[[ -f "$1" ]] && source "$1"
+}
+
+safe_source ~/.iterm2_shell_integration.zsh
+
+unalias run-help
+autoload run-help
+HELPDIR=/usr/local/share/zsh/help
+
+# functions
+has() {
+	type "$1" >/dev/null 2>/dev/null
+}
+
+setdevice() {
+	if [[ "$1" == "" ]]; then
+		echo "Usage: `basename "$0"` device"
+		return 1
+	fi
+
+	[[ "$1" == "local" ]] && export THEOS_DEVICE_PORT=2222 || THEOS_DEVICE_PORT=22
+	export THEOS_DEVICE_IP=$1
+
+	echo "$THEOS_DEVICE_IP:$THEOS_DEVICE_PORT"
+}
+
+xcswitch() {
+	xcpath=/Applications/Xcode$1.app
+	sudo xcode-select -switch "${xcpath}/Contents/Developer"
+	echo "Switched to $(xcsel -print-path)"
+}
+
+
+# not found handler
+command_not_found_handler() {
+	echo "$1: command not found"
+
+	if [[ -x /usr/lib/command-not-found ]]; then
+		/usr/lib/command-not-found --no-failure-msg -- "$1"
+	elif has brew; then
+		brew which-formula --explain "$1"
+	fi
+}
 
 # exports
 export PROJ=~/Documents/Projects
-export THEOS_DEVICE_IP=local THEOS_DEVICE_PORT=2222
-export SDKVERSION= SIMVERSION=
+PROJ=~PROJ # this amusing little hack shortens the prompt from /Users/kirb/Documents/Projects to ~PROJ
 
-export EDITOR='subl -w'
-[[ ! -z "$SSH_CLIENT" ]] && export EDITOR=nano
+export THEOS_DEVICE_IP=local THEOS_DEVICE_PORT=2222
+
+if has subl && [[ -z "$SSH_CLIENT" ]]; then
+	export EDITOR='subl -w'
+else
+	export EDITOR=nano
+fi
 
 export PERL_MB_OPT="--install_base \"$HOME/.perl5\""
 export PERL_MM_OPT="INSTALL_BASE=$HOME/.perl5"
 export GOPATH=/usr/local/lib/go
 
-[[ -z "$IPHONEOS_ROOT" ]] && export IPHONEOS_ROOT=~/sdks/ios/iPhoneOS.sdk
-[[ -z "$IPHONE_SIMULATOR_ROOT" ]] && export IPHONE_SIMULATOR_ROOT=~/sdks/sim/iPhoneSimulator.sdk
-export SDK="$IPHONEOS_ROOT" SIM="$IPHONE_SIMULATOR_ROOT"
-
+export IPHONEOS_ROOT=~/sdks/ios/iPhoneOS.sdk
+export IPHONE_SIMULATOR_ROOT=~/sdks/sim/iPhoneSimulator.sdk
 
 # aliases
-alias ls='ls -Fblapht --color'
+alias ls='ls -Fbapht --color'
+alias lsd='ls -l'
 alias grep='grep --color -rin --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
 alias nic='$THEOS/bin/nic.pl'
 alias logify='$THEOS/bin/logify.pl'
@@ -96,35 +146,10 @@ alias cr='open -b com.google.Chrome'
 alias vlc='open -b org.videolan.vlc'
 alias strings='strings -a'
 alias hop='hopper -e'
+alias optim='open -b net.pornel.ImageOptim'
+alias io=optim
+alias alpha='open -b net.pornel.ImageAlpha'
 
-# functions
-setdevice() {
-	if [[ "$1" == "" ]]; then
-		echo "Usage: `basename "$0"` device"
-		return 1
-	fi
-
-	[[ "$1" == "local" ]] && export THEOS_DEVICE_PORT=2222 || THEOS_DEVICE_PORT=22
-	export THEOS_DEVICE_IP=$1
-
-	echo "$THEOS_DEVICE_IP:$THEOS_DEVICE_PORT"
-}
-
-xcswitch() {
-	xcpath=/Applications/Xcode$1.app
-	sudo xcode-select -switch "${xcpath}/Contents/Developer"
-	echo "Switched to $(xcsel -print-path)"
-}
-
-xcopen() {
-	# holy shit this is terrible
-	if test -n "$(find . -maxdepth 1 -name '*.xcworkspace' -print -quit)"; then
-		echo "Opening workspace"
-		open *.xcworkspace
-	elif test -n "$(find . -maxdepth 1 -name '*.xcodeproj' -print -quit)"; then
-		echo "Opening project"
-		open *.xcodeproj
-	else
-		echo "Nothing found"
-	fi
-}
+# this must be sourced last
+safe_source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+safe_source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
